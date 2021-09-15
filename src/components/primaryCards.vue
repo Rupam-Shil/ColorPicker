27174bdf25efdf12f31a0c317ee99f0deee85e30
Rupam Shil <rupamshil111@gmail.com>
2021-09-15 12:02:45 +0530

    color copy to clipboard done

--- src/components/primaryCards.vue
+++ src/components/primaryCards.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="card">
-		<div class="card-top" :style="{ backgroundColor: props.colorCode }"></div>
+		<div class="card-top" :style="{ backgroundColor: props.colorCode }">
+			<button @click="copyToClipBoard"></button>
+		</div>
 		<div class="card-bottom">
 			<div class="color-name">{{ props.colorName }}</div>
 			<div class="color-code">{{ props.colorCode }}</div>
@@ -9,10 +11,27 @@
 </template>
 
 <script setup>
+import { defineProps, defineEmits } from 'vue';
+import useClipboard from 'vue-clipboard3';
+
 const props = defineProps({
 	colorCode: String,
 	colorName: String,
 });
+
+const emits = defineEmits(['copied']);
+//Clipboard Function
+
+const { toClipboard } = useClipboard();
+
+const copyToClipBoard = async () => {
+	try {
+		await toClipboard(props.colorCode);
+		emits('copied');
+	} catch (e) {
+		console.log(e);
+	}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -28,6 +47,14 @@ const props = defineProps({
 		width: 100%;
 		height: 70%;
 		border-radius: 1rem 1rem 0 0;
+		button {
+			height: 100%;
+			width: 100%;
+			background: transparent;
+			outline: none;
+			border: none;
+			cursor: pointer;
+		}
 	}
 	&-bottom {
 		height: 30%;
@@ -36,7 +63,9 @@ const props = defineProps({
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
+		box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.3);
 		.color-name {
+			font-weight: 600;
 		}
 	}
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
 	<div class="home">
+		<transition @enter="enter">
+			<div class="copyToClipboard" v-if="isCopied">Copied 📋</div>
+		</transition>
 		<section class="primary ">
 			<h1 class="header mb-1">Color</h1>
 			<p class="sub-heading mb-2">
@@ -7,17 +10,56 @@
 				of the primary blue and content to take center stage.
 			</p>
 			<div class="primary-card-section grid-col-4">
-				<primaryCards />
-				<primaryCards />
-				<primaryCards />
-				<primaryCards />
+				<primaryCards
+					v-for="(card, index) in primaryCardColors"
+					:key="index"
+					:colorCode="card.colorCode"
+					:colorName="card.colorName"
+					@copied="copiedToClipboard"
+				/>
 			</div>
 		</section>
 	</div>
 </template>
 
 <script setup>
+import gsap from 'gsap';
 import primaryCards from '../components/primaryCards.vue';
+import { ref } from 'vue';
+
+let isCopied = ref(false);
+
+const primaryCardColors = [
+	{ colorName: 'Charcoal', colorCode: '#383F48' },
+	{ colorName: 'Primary Blue', colorCode: '#06BEE1' },
+	{ colorName: 'Base Grey', colorCode: '#E1E1E1' },
+	{ colorName: 'White', colorCode: '#FFFFFF' },
+];
+//gsap
+
+function copiedToClipboard() {
+	isCopied.value = true;
+}
+
+const enter = (el, done) => {
+	gsap.to(el, {
+		x: -100,
+		ease: 'elastic.inOut(2.5, 1)',
+		opacity: 1,
+		duration: 1,
+	});
+	gsap.to(el, {
+		y: 100,
+		ease: 'elastic.inOut(2.5, 1)',
+		opacity: 0,
+		duration: 1,
+		delay: 1,
+		onComplete: () => {
+			isCopied.value = false;
+			done();
+		},
+	});
+};
 </script>
 
 <style lang="scss" scoped>
@@ -47,6 +89,21 @@ section.primary {
 	grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
 	gap: 2rem;
 	justify-items: center;
+}
+.copyToClipboard {
+	background: #fff;
+	padding: 0.8rem 2rem;
+	position: fixed;
+	bottom: 10px;
+	right: 0;
+	opacity: 0;
+	color: var(--text-color-dark);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border-radius: 5px;
+	font-size: 1.5rem;
+	font-weight: 600;
 }
 
 @media only screen and (max-width: 900px) {
