0a50a97704edbe4491f4779fe069520399be3879
Adhi2648 <adhinaryanan619@gmail.com>
2021-09-17 17:30:22 +0530

    palette added

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
-import Gradient from '../views/Gradient.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Gradient from "../views/Gradient.vue";
+import Palette from "../views/palette.vue";
 
 const routes = [
-	{
-		path: '/',
-		name: 'Home',
-		component: Home,
-	},
-	{
-		path: '/gradient',
-		name: 'Gradient',
-		component: Gradient,
-	},
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/gradient",
+    name: "Gradient",
+    component: Gradient,
+  },
+  {
+    path: "/palette",
+    name: "Palette",
+    component: Palette,
+  },
 ];
 
 const router = createRouter({
-	history: createWebHistory(process.env.BASE_URL),
-	routes,
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
 });
 
 export default router;
---
+++ src/views/palette.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    this is apaette
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
